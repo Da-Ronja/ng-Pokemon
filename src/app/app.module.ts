de93d8f12e62265fc49ab976d9a6cb6abc2f938e
Jakob-mbi <jakob_mbi@msn.com>
2023-01-31 21:18:27 +0100

    feat: add routing for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
+
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    TrainerPage,
+    PokemonCataloguePage,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],

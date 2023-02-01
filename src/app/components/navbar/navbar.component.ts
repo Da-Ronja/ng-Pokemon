bc3a598d4c817cf986b7d0223cbd13a2f46cae70
Jakob-mbi <jakob_mbi@msn.com>
2023-02-01 16:41:11 +0100

    feat: navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AuthorizationGuard } from 'src/app/guards/authorization.guard';
+import { TrainerService } from 'src/app/services/trainer.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+}

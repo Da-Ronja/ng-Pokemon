379d5a89ae84d4864f83c1fa153fbfd23605d0f6
Jakob-mbi <jakob_mbi@msn.com>
2023-02-01 13:46:28 +0100

    feat: add trainer.service

---
+++ src/app/services/trainer.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { storageKeys } from '../enum/storage-keys.enum';
+import { Trainer } from '../models/trainer.model';
+import { StorageUtil } from '../utils/storage.Utils';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrainerService {
+  // keep track of login trainer
+  private _trainer?: Trainer;
+
+  get trainer(): Trainer | undefined {
+    return this._trainer;
+  }
+  set trainer(trainer: Trainer | undefined) {
+    this._trainer = trainer;
+  }
+
+  constructor() { 
+    this._trainer = StorageUtil.storageRead<Trainer>(storageKeys.Trainer);
+
+  }
+}
